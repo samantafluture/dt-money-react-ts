b760d73e28ce19a502a46c3e762a3fb093582f95
Samanta Fluture <samantafluture@gmail.com>
2022-10-11 15:33:58 -0400

    create header and summary component and style

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,30 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryContainer, SummaryCard } from "./styles";
+
+export function Summary() {
+  return(
+   <SummaryContainer>
+    <SummaryCard>
+      <header>
+        <span>Income</span>
+        <ArrowCircleUp size={32} color="#00B37E" />
+      </header>
+      <strong>CAD$ 17,400.00</strong>
+    </SummaryCard>
+    <SummaryCard>
+      <header>
+        <span>Expense</span>
+        <ArrowCircleDown size={32} color="#F75A68" />
+      </header>
+      <strong>CAD$ 17,400.00</strong>
+    </SummaryCard>
+    <SummaryCard variant="green">
+      <header>
+        <span>Total</span>
+        <CurrencyDollar size={32} color="#FFF" />
+      </header>
+      <strong>CAD$ 17,400.00</strong>
+    </SummaryCard>
+   </SummaryContainer>
+  )
+}

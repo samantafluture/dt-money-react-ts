6ba6968eea09a971c4af1a954fd7065851f8d653
Samanta Fluture <samantafluture@gmail.com>
2022-10-12 11:07:16 -0400

    using query params to search into json server

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
@@ -16,18 +16,20 @@ const newTransactionFormSchema = z.object({
 	description: z.string(),
 	price: z.number(),
 	category: z.string(),
-	// type: z.enum(['income', 'expense']),
+	type: z.enum(['income', 'expense']),
 })
 
 type newTransactionFormInputs = z.infer<typeof newTransactionFormSchema>
 
 export function NewTransactionModal() {
 	const {
+		control,
 		register,
 		handleSubmit,
 		formState: { isSubmitting },
 	} = useForm<newTransactionFormInputs>({
 		resolver: zodResolver(newTransactionFormSchema),
+		defaultValues: { type: 'income' },
 	})
 
 	function handleCreateNewTransaction(data: newTransactionFormInputs) {
@@ -62,21 +64,37 @@ export function NewTransactionModal() {
 						{...register('category')}
 					/>
 
-					<TransactionType>
-						<TransactionTypeButton variant='income' value='income'>
-							<ArrowCircleUp size={24} />
-							Income
-						</TransactionTypeButton>
-						<TransactionTypeButton
-							variant='expense'
-							value='expense'
-						>
-							<ArrowCircleDown size={24} />
-							Expense
-						</TransactionTypeButton>
-					</TransactionType>
+					<Controller
+						control={control}
+						name='type'
+						render={({ field }) => {
+							return (
+								<TransactionType
+									onValueChange={field.onChange}
+									value={field.value}
+								>
+									<TransactionTypeButton
+										variant='income'
+										value='income'
+									>
+										<ArrowCircleUp size={24} />
+										Income
+									</TransactionTypeButton>
+									<TransactionTypeButton
+										variant='expense'
+										value='expense'
+									>
+										<ArrowCircleDown size={24} />
+										Expense
+									</TransactionTypeButton>
+								</TransactionType>
+							)
+						}}
+					/>
 
-					<button type='submit' disabled={isSubmitting}>Create</button>
+					<button type='submit' disabled={isSubmitting}>
+						Create
+					</button>
 				</form>
 			</Content>
 		</Dialog.Portal>
--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SearchFormContainer } from './styles'
 import * as z from 'zod'
+import { useContext } from 'react'
+import { TransactionsContext } from '../../../../contexts/TransactionsContext'
 
 const searchFormSchema = z.object({
 	query: z.string(),
@@ -11,6 +13,8 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+	const { fetchTransactions } = useContext(TransactionsContext)
+	
 	const {
 		register,
 		formState: { isSubmitting },
@@ -19,8 +23,8 @@ export function SearchForm() {
 		resolver: zodResolver(searchFormSchema),
 	})
 
-	function handleSearchTransactions(data: SearchFormInputs) {
-		console.log(data)
+	async function handleSearchTransactions(data: SearchFormInputs) {
+		await fetchTransactions(data.query)
 	}
 
 	return (
